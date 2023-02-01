834c62b38f96f6b546dd099cbe46872b28eac179
FabiDiasL <fabi_lima_@hotmail.com>
2023-02-01 19:01:05 -0300

    update

--- src/pilha_numerica/Interacao_Pilha.java
+++ src/pilha_numerica/Interacao_Pilha.java
@@ -1,10 +1,60 @@
 package pilha_numerica;
 
+import java.util.Scanner;
+
 public class Interacao_Pilha {
 
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
+		
+		Pilha pilha = new Pilha();
+		
+		int opc = 0;
+		
+		while(opc != 5) {
+			
+		
+		System.out.println("");
+		System.out.print("1 - Inserir");
+		System.out.print("2 - Listar");
+		System.out.print("3 - Pesquisar");
+		System.out.print("4 - Excluir");
+		System.out.print("5 - Sair");
+		System.out.println("");
+		System.out.print("Escolha uma opção: ");
+		opc = sc.nextInt();
+		
+		switch (opc){
+		
+		case 1:
+			//inserir();
+			break;
+			
+		case 2:
+			break;
+			
+		case 3:
+			break;
+		
+		case 4:
+			break;
+		
+		case 5:
+			System.out.println("Finalizado com sucesso!");
+			break;
+			
+		default:
+			System.out.println("Opção inválida!");
+			break;
+		}
+		
+		
+		
+		}
 
 	}
+
+	
 
 }
--- src/pilha_numerica/Pilha.java
+++ src/pilha_numerica/Pilha.java
@@ -1,6 +1,34 @@
 package pilha_numerica;
 
+import java.util.Scanner;
+
 public class Pilha {
+	
+	Scanner sc = new Scanner(System.in);
+	
+	int vPilha[] = new int[50];
+	private int qtd = 0;
+	
+	public Pilha() {
+		this.qtd = 0;
+	}
+
+	public int getQtd() {
+		return qtd;
+	}
+
+	public void setQtd(int qtd) {
+		this.qtd = qtd;
+	}
+	
+	private static void inserir() {
+		
+		
+	}
+	
+	
+	
+	
 	
 	
 
