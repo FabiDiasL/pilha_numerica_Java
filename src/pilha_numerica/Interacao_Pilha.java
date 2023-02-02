0fbfe62fe81c67dfdf803c902b38392e67910442
FabiDiasL <fabi_lima_@hotmail.com>
2023-02-02 19:44:19 -0300

    update

--- src/pilha_numerica/Interacao_Pilha.java
+++ src/pilha_numerica/Interacao_Pilha.java
@@ -5,56 +5,53 @@ import java.util.Scanner;
 public class Interacao_Pilha {
 
 	public static void main(String[] args) {
-		
+
 		Scanner sc = new Scanner(System.in);
-		
+
 		Pilha pilha = new Pilha();
-		
+
 		int opc = 0;
-		
-		while(opc != 5) {
-			
-		
-		System.out.println("");
-		System.out.print("1 - Inserir");
-		System.out.print("2 - Listar");
-		System.out.print("3 - Pesquisar");
-		System.out.print("4 - Excluir");
-		System.out.print("5 - Sair");
-		System.out.println("");
-		System.out.print("Escolha uma opção: ");
-		opc = sc.nextInt();
-		
-		switch (opc){
-		
-		case 1:
-			//inserir();
-			break;
-			
-		case 2:
-			break;
-			
-		case 3:
-			break;
-		
-		case 4:
-			break;
-		
-		case 5:
-			System.out.println("Finalizado com sucesso!");
-			break;
-			
-		default:
-			System.out.println("Opção inválida!");
-			break;
-		}
-		
-		
-		
-		}
 
+		while (opc != 5) {
+
+			System.out.println("");
+			System.out.println("Pilha numérica");
+			System.out.println("");
+			System.out.println("1 - Inserir");
+			System.out.println("2 - Listar");
+			System.out.println("3 - Pesquisar");
+			System.out.println("4 - Excluir");
+			System.out.println("5 - Sair");
+			System.out.println("");
+			System.out.print("Digite a opção desejada: ");
+			opc = sc.nextInt();
+
+			switch (opc) {
+
+			case 1:
+				pilha.inserir();
+				break;
+
+			case 2:
+				pilha.listar();
+				break;
+
+			case 3:
+				break;
+
+			case 4:
+				break;
+
+			case 5:
+				System.out.println("Finalizado com sucesso!");
+				break;
+
+			default:
+				System.out.println("Opção inválida!");
+				break;
+			}
+
+		}
+		sc.close();
 	}
-
-	
-
 }
--- src/pilha_numerica/Pilha.java
+++ src/pilha_numerica/Pilha.java
@@ -3,33 +3,50 @@ package pilha_numerica;
 import java.util.Scanner;
 
 public class Pilha {
-	
+
 	Scanner sc = new Scanner(System.in);
-	
+
 	int vPilha[] = new int[50];
-	private int qtd = 0;
-	
+	int qtd = 0;
+
 	public Pilha() {
+		this.vPilha = vPilha;
 		this.qtd = 0;
 	}
 
-	public int getQtd() {
-		return qtd;
+	void inserir() {
+		int valor;
+		if (!pilhaCheia()) {
+			System.out.print("Digite o número que deseja inserir na pilha numérica: ");
+			valor = sc.nextInt();
+			vPilha[qtd] = valor;
+			qtd++;
+			System.out.println("Número " +valor+" inserido com sucesso!");
+		}
 	}
 
-	public void setQtd(int qtd) {
-		this.qtd = qtd;
+	void listar() {
+		if (!pilhaVazia()) {
+			for (int i = 0; i < qtd; i++) {
+				System.out.println((i + 1) + "º elemento: " + vPilha[i]);
+			}
+		}
 	}
-	
-	private static void inserir() {
-		
-		
-	}
-	
-	
-	
-	
-	
-	
 
+	private boolean pilhaCheia() {
+		if (qtd == vPilha.length) {
+			System.out.println("A pilha está cheia!");
+			return true;
+		}
+		return false;
+	}
+
+	private boolean pilhaVazia() {
+		if (qtd == 0) {
+			System.out.println("A pilha está vazia!");
+			return true;
+		} else {
+			return false;
+		}
+	}
 }
